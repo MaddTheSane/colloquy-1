3f220f9016519ace144284114ccaab3b3a121e60
C.W. Betts <computers57@hotmail.com>
2016-06-12 13:52:31 -0600

    Have an iOS framework for ChatCore.

--- Chat Core/ChatCore.h
+++ Chat Core/ChatCore.h
@@ -1,3 +1,5 @@
+#include <Availability.h>
+
 #import <Foundation/Foundation.h>
 
 #import <ChatCore/MVAvailability.h>
@@ -10,14 +12,21 @@
 #import <ChatCore/MVChatUserWatchRule.h>
 #import <ChatCore/MVFileTransfer.h>
 #import <ChatCore/MVDirectChatConnection.h>
-#import <ChatCore/MVChatPluginManager.h>
 
-#import <ChatCore/NSAttributedStringAdditions.h>
+#if !(TARGET_OS_IOS || TARGET_OS_WATCH || TARGET_OS_TV)
+#import <ChatCore/MVChatPluginManager.h>
+#endif
+
+
 #import <ChatCore/NSStringAdditions.h>
 #import <ChatCore/NSNumberAdditions.h>
-#import <ChatCore/NSColorAdditions.h>
 #import <ChatCore/NSDataAdditions.h>
 #import <ChatCore/NSScannerAdditions.h>
-#import <ChatCore/NSMethodSignatureAdditions.h>
 #import <ChatCore/NSNotificationAdditions.h>
+
+#if !(TARGET_OS_IOS || TARGET_OS_WATCH || TARGET_OS_TV)
+#import <ChatCore/NSAttributedStringAdditions.h>
+#import <ChatCore/NSColorAdditions.h>
+#import <ChatCore/NSMethodSignatureAdditions.h>
 #import <ChatCore/NSScriptCommandAdditions.h>
+#endif
