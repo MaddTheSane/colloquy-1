a45b752d4ee128f2722b1744a578317aac0ada44
C.W. Betts <computers57@hotmail.com>
2017-10-07 01:27:06 -0600

    Add a substring regex Swift matcher.

--- Additions/NSStringAdditions_Swift.swift
+++ Additions/NSStringAdditions_Swift.swift
@@ -42,4 +42,18 @@ extension String {
 		}
 		return convRange
 	}
+	
+	/// - parameter regex: The regular expression to search for.
+	/// - parameter options: The regular expression options.<br>
+	/// Default is `[.useUnicodeWordBoundaries]`.
+	/// - parameter range1: The range to search for the regex. If `nil`,
+	/// searches the whole string.<br>
+	/// Default is `nil`
+	/// - parameter capture: Which capture to use.<br>
+	/// Default is `0`.
+	public func match(regex: String, options: NSRegularExpression.Options = [.useUnicodeWordBoundaries], in range1: Range<String.Index>? = nil, capture: Int = 0) throws -> Substring {
+		let newRange = try range(ofRegex: regex, options: options, in: range1, capture: capture)
+		let subStr = self[newRange]
+		return subStr
+	}
 }
