bd858a08ac955e21d6f256557bfdab4632b9a24f
C.W. Betts <computers57@hotmail.com>
2017-10-29 14:42:29 -0600

    Minor Swift refinements.

--- Controllers/JVSpeechController.swift
+++ Controllers/JVSpeechController.swift
@@ -8,6 +8,10 @@
 
 import Cocoa
 
+/// Limit the number of outstanding strings to 15. This will prevent massive amounts of TTS flooding
+/// when you get a channel flood or re-connect to a dircproxy server.
+private let maxSpeechQueue = 15
+
 @objc(JVSpeechController)
 public class JVSpeechController: NSObject, NSSpeechSynthesizerDelegate {
 	@objc(sharedSpeechController)
@@ -15,18 +19,18 @@ public class JVSpeechController: NSObject, NSSpeechSynthesizerDelegate {
 	private var speechQueue = [(text: String, voice: NSSpeechSynthesizer.VoiceName)]()
 	private let synthesizers: [NSSpeechSynthesizer]
 
-	override init() {
+	private override init() {
 		synthesizers = [NSSpeechSynthesizer(voice: nil)!, NSSpeechSynthesizer(voice: nil)!, NSSpeechSynthesizer(voice: nil)!]
 		super.init()
 		
 		for synthesizer in synthesizers {
 			synthesizer.delegate = self
 		}
-		speechQueue.reserveCapacity(15)
+		speechQueue.reserveCapacity(maxSpeechQueue)
 	}
 	
 	@objc(startSpeakingString:usingVoice:)
-	public func startSpeaking(_ string: String, usingVoice voice: NSSpeechSynthesizer.VoiceName) {
+	public func startSpeaking(_ string: String, using voice: NSSpeechSynthesizer.VoiceName) {
 		for synth in synthesizers {
 			if !synth.isSpeaking {
 				synth.setVoice(voice)
@@ -38,7 +42,7 @@ public class JVSpeechController: NSObject, NSSpeechSynthesizerDelegate {
 		// Limit the number of outstanding strings to 15. This will prevent massive amounts of TTS flooding
 		// when you get a channel flood or re-connect to a dircproxy server. Remove the oldest string from
 		// the queue and then insert the new string onto the end.
-		if speechQueue.count > 15 {
+		if speechQueue.count > maxSpeechQueue {
 			speechQueue.remove(at: 0)
 		}
 		
@@ -46,10 +50,11 @@ public class JVSpeechController: NSObject, NSSpeechSynthesizerDelegate {
 	}
 	
 	public func speechSynthesizer(_ sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
-		if speechQueue.count != 0  {
-			let nextSpeech = speechQueue.removeFirst()
-			sender.setVoice(nextSpeech.voice)
-			sender.startSpeaking(nextSpeech.text)
+		guard speechQueue.count != 0 else {
+			return
 		}
+		let nextSpeech = speechQueue.removeFirst()
+		sender.setVoice(nextSpeech.voice)
+		sender.startSpeaking(nextSpeech.text)
 	}
 }
