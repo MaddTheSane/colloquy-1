38e02d1d679095cbb8eb09ca7c42c4127d9b619b
C.W. Betts <computers57@hotmail.com>
2017-11-12 21:15:27 -0700

    Change NSCharacterSet's class property encodedXMLCharacterSet to include the cq_ prefix.

--- Additions/NSCharacterSetAdditions.h
+++ Additions/NSCharacterSetAdditions.h
@@ -2,7 +2,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSCharacterSet (Additions)
 @property (class, readonly, strong) NSCharacterSet *illegalXMLCharacterSet;
-@property (class, readonly, strong, getter=cq_encodedXMLCharacterSet) NSCharacterSet *encodedXMLCharacterSet;
+@property (class, readonly, strong, getter=cq_encodedXMLCharacterSet) NSCharacterSet *cq_encodedXMLCharacterSet;
 @end
 
 NS_ASSUME_NONNULL_END
