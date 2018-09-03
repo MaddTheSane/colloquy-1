e09bae3601a6b10832e34c4a3d3f9371d67aaaa2
C.W. Betts <computers57@hotmail.com>
2018-09-02 18:49:09 -0600

    Add Swift version of replaceOccurrences(ofRegex:...) for String.

--- Additions/NSStringAdditions_Swift.swift
+++ Additions/NSStringAdditions_Swift.swift
@@ -9,12 +9,11 @@
 import Foundation
 
 extension NSString {
-	@nonobjc public func range(ofRegex regex: String, options: NSRegularExpression.Options, in range: NSRange, capture: Int) throws -> NSRange {
+	@nonobjc public func range(ofRegex regex: String, options: NSRegularExpression.Options = [.useUnicodeWordBoundaries], in range: NSRange, capture: Int = 0) throws -> NSRange {
 		var errPtr: NSError? = nil
-		let regRange = self.range(ofRegex: regex, options: options, in: range, capture: capture, error: &errPtr)
+		let regRange = self.__range(ofRegex: regex, options: options, in: range, capture: capture, error: &errPtr)
 		if regRange.location == NSNotFound {
-			//TODO better fallback
-			throw errPtr ?? NSError(domain: NSCocoaErrorDomain, code: -1, userInfo: nil)
+			throw errPtr ?? CocoaError(.formatting)
 		}
 		return regRange
 	}
@@ -32,13 +31,9 @@ extension String {
 	public func range(ofRegex regex: String, options: NSRegularExpression.Options = [.useUnicodeWordBoundaries], in range1: Range<String.Index>? = nil, capture: Int = 0) throws -> Range<String.Index> {
 		let range = range1 ?? startIndex ..< endIndex
 		let regRange = try (self as NSString).range(ofRegex: regex, options: options, in: NSRange(range, in: self), capture: capture)
-		if regRange.location == NSNotFound {
-			//TODO better fallback
-			throw NSError(domain: NSCocoaErrorDomain, code: -1, userInfo: nil)
-		}
 		
 		guard let convRange = Range(regRange, in: self) else {
-			throw NSError(domain: NSCocoaErrorDomain, code: NSFormattingError, userInfo: nil)
+			throw CocoaError(.formatting)
 		}
 		return convRange
 	}
@@ -55,5 +50,17 @@ extension String {
 		let newRange = try range(ofRegex: regex, options: options, in: range1, capture: capture)
 		let subStr = self[newRange]
 		return subStr
+	}
+	
+	/// - parameter regex: The regular expression to search for.
+	/// - parameter options: The regular expression options.<br>
+	/// Default is `[.useUnicodeWordBoundaries]`.
+	/// - parameter range1: The range to search for the regex. If `nil`,
+	/// searches the whole string.<br>
+	/// Default is `nil`
+	/// - parameter replacement: What to replace the matched regex with.
+	public mutating func replaceOccurrences(ofRegex regex: String, with replacement: String, options: NSRegularExpression.Options = [.useUnicodeWordBoundaries], in range1: Range<String.Index>? = nil) throws {
+		let newRange = try range(ofRegex: regex, options: options, in: range1)
+		self.replaceSubrange(newRange, with: replacement)
 	}
 }
